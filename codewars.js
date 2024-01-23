1e7a8b573fd2bb452c4e1c1582f4a2d2b4fad98a
pdv@sinergium.ru <pdv@sinergium.ru>
2024-01-23 18:55:42 +0300

    7-8kyu

--- codewars.js
+++ codewars.js
@@ -39,3 +39,49 @@ function countPositivesSumNegatives(input) {
 }
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
+function reverseWords(str) {
+    arr = str.split(' ')
+    arrNew = []
+
+    arr.forEach(e => {
+        newStr = e.split('').reverse().join('')
+        arrNew.push(newStr)
+    })
+
+    console.log(arrNew.join(' '))
+}
+
+reverseWords("This is an example!")
+
+
+const rps = (p1, p2) => {
+
+    if (p1 == p2) {
+        console.log("Draw!")
+    } else if (p1 == "scissors") {
+        if (p2 == "paper") {
+            console.log('Player 1 won!')
+        } else {
+            console.log('Player 2 won!')
+        }
+    } else if (p1 == "paper") {
+        if (p2 == "scissors") {
+            console.log('Player 2 won!')
+        } else {
+            console.log('Player 1 won!')
+        }
+    } else {
+        if (p2 == "scissors") {
+            console.log('Player 1 won!')
+        } else {
+            console.log('Player 2 won!')
+        }
+    }
+};
+
+
+rps("scissors", "paper")
+rps("scissors", "rock")
+rps("paper", "rock")
+rps("paper", "paper")
